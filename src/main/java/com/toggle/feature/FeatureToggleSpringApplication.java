f1e7ea6aebf9f62ebbc7016e0ec98ee1bc95b176
math-ferreira <mat.s.ferreira@gmail.com>
2023-08-15 18:40:59 +0100

    first commit

---
+++ src/main/java/com/toggle/feature/FeatureToggleSpringApplication.java
@@ -0,0 +1,13 @@
+package com.toggle.feature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureToggleSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatureToggleSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toggle/feature/FeatureToggleSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toggle.feature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureToggleSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
